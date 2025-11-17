4a6d05a3482827719bf0d1876c018c34fa179c9b
Carolina Valladolid <carolinavalladolidbravo@gmail.com>
2025-11-17 11:54:09 -0500

    Ejemplo de Stack

--- src/main/java/com/mycompany/stack/Stack.java
+++ src/main/java/com/mycompany/stack/Stack.java
@@ -15,7 +15,9 @@ public class Stack {
         pila1.push(5);
         pila1.push(7);
         pila1.push(1);
-         System.out.println("Pop: " + pila1.pop());    
+        System.out.println("Elementos de la pila:");
+        pila1.mostrarElementos();  
+        System.out.println("Pop: " + pila1.pop());    
         System.out.println("Peek: " + pila1.peek());   
         System.out.println("Esta vacia: " + pila1.isEmpty());
         System.out.println("Elementos de la pila:");
